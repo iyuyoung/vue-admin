bb53a113eed6f0098ea6ffc9d8043de6418f10f4
Yuyoung <892887001@qq.com>
2020-08-11 10:35:58 +0800

    修改打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?
-    '/static/admin/dist/' : '/',
+    '/dist/' : '/',
   outputDir: process.env.NODE_ENV === 'production' ?
-    '/static/admin/dist/' : '/',
+    '/dist/' : '/',
   devServer: {
     disableHostCheck: true,
     proxy: {
